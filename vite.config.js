785b8e4cefc787b2525def2de74e7867d6b8b9e5
OtiliaNicola <162286245+OtiliaNicola@users.noreply.github.com>
2025-01-08 15:53:24 +0100

    Update base vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/modulo-3-evaluacion-final-OtiliaNicola/',
+  base: '/',
   build: {
     minify: 'esbuild',
     outDir: './dist',
