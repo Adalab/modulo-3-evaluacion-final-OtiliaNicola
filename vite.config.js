20acaf43a4984a6fa8b432428e8d845a3cd9b458
OtiliaNicola <162286245+OtiliaNicola@users.noreply.github.com>
2025-01-08 11:57:32 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,25 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/modulo-3-evaluacion-final-OtiliaNicola/'
+  base:'/modulo-3-evaluacion-final-OtiliaNicola/',
+   build: {
+    minify: "esbuild",
+    outDir: "../dist",
+    sourcemap: "inline",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: inputFilesList,
+      output: {
+        sourcemap: true,
+        entryFileNames: ({name}) => {
+          if( name === 'main' ) {
+            return 'js/main.js';
+          }
+          // default value
+          // ref: https://rollupjs.org/configuration-options/#output-entryfilenames
+          return "[name].js";
+        },
+      },
+    },
+  },
 })
